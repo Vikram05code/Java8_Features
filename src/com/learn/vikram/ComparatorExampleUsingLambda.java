2f6b4c1fa8cbc91c26a3902f13f357dc1e384558
Vikram <vikky5vikram@gmail.com>
2024-11-09 23:30:08 +0530

    Implement Comparatr using Lambda

---
+++ src/com/learn/vikram/ComparatorExampleUsingLambda.java
@@ -0,0 +1,26 @@
+package com.learn.vikram;
+
+import java.util.Comparator;
+
+public class ComparatorExampleUsingLambda {
+
+    public static void main(String[] args) {
+
+        //prior java 8
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer ob1, Integer ob2) {
+                return ob1.compareTo(ob2); //return 0 if equal, return 1 if ob1>ob2, return -1 if ob1<ob2
+
+            }
+        };
+        System.out.println("Result of Comparator is: "+comparator.compare(35,30));
+
+        //after java 8
+        Comparator<Integer> comparatorLambda = (Integer ob1, Integer ob2)->ob1.compareTo(ob2);
+        System.out.println("Result of Comparator using Lambda is: "+comparatorLambda.compare(35,30));
+
+        Comparator<Integer> comparatorLambda1 = (ob1, ob2)->ob1.compareTo(ob2);
+        System.out.println("Result of Comparator using Lambda is: "+comparatorLambda1.compare(40,30));
+    }
+}
