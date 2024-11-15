f0b8ac751aaeebf54942a27c1a27d17b57328da4
Vikram <vikky5vikram@gmail.com>
2024-11-15 18:20:37 +0530

    Use Case of andThen() method in Consumer Functional Interface

--- src/com/learn/vikram/functionalInterfaces/ConsumerExample.java
+++ src/com/learn/vikram/functionalInterfaces/ConsumerExample.java
@@ -14,12 +14,21 @@ public class ConsumerExample {
         studentList.forEach(consumer1);
     }
 
+    public static void printNameAndActivities(){
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        Consumer<Student> consumer2 = (student)->System.out.print(student.getName());
+        Consumer<Student> consumer3 = (student)->System.out.println(student.getActivities());
+        studentList.forEach(consumer2.andThen(consumer3));
+    }
+
+
     public static void main(String[] args){
 
         Consumer<String> consumer1 = s->System.out.println(s.toUpperCase());
         consumer1.accept("vikram");
 
         printStudents();
+        printNameAndActivities();
 
     }
 
