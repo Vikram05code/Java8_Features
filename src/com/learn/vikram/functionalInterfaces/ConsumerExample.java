694df37966b67714e5b1ae48340cd40266d69507
Vikram <vikky5vikram@gmail.com>
2024-11-15 17:49:35 +0530

    Using Consumer Functional interface printing students names from studentList

--- src/com/learn/vikram/functionalInterfaces/ConsumerExample.java
+++ src/com/learn/vikram/functionalInterfaces/ConsumerExample.java
@@ -1,13 +1,27 @@
 package com.learn.vikram.functionalInterfaces;
 
+import com.learn.vikram.data.Student;
+import com.learn.vikram.data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 
+    public static void printStudents(){
+        Consumer<Student> consumer1 = (student)->System.out.println(student);
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(consumer1);
+    }
+
     public static void main(String[] args){
 
-        Consumer<String> consumer = s->System.out.println(s.toUpperCase());
-        consumer.accept("vikram");
+        Consumer<String> consumer1 = s->System.out.println(s.toUpperCase());
+        consumer1.accept("vikram");
+
+        printStudents();
 
     }
+
+
 }
