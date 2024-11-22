d9e1de400daf259467c6f7562e27ef513e62f9dc
Vikram <vikky5vikram@gmail.com>
2024-11-22 21:44:32 +0530

    filter Student By GPA using Predicate

--- src/com/learn/vikram/functionalInterfaces/PredicateStudentExample.java
+++ src/com/learn/vikram/functionalInterfaces/PredicateStudentExample.java
@@ -10,7 +10,10 @@ public class PredicateStudentExample {
 
    static Predicate<Student> p1 = student -> student.getGradeLevel()>=3;
 
+   static Predicate<Student> p2 = student -> student.getGpa()>=3.9;
+
     public static void filterStudentByGradeLevel(){
+        System.out.println("-------------filterStudentByGradeLevel-------------");
         List<Student> studentList = StudentDataBase.getAllStudents();
         studentList.forEach(student->{
             if(p1.test(student)){
@@ -19,8 +22,19 @@ public class PredicateStudentExample {
         });
     }
 
+    public static void filterStudentByGPA(){
+        System.out.println("-------------filterStudentByGPA-------------");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(student->{
+            if(p2.test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
 
     public static void main(String[] args){
         filterStudentByGradeLevel();
+        filterStudentByGPA();
     }
 }
