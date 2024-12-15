368e899617c367527f25e8b4082880ed6b2a64ae
Vikram <vikky5vikram@gmail.com>
2024-12-15 14:20:15 +0530

    BiFunction Functional Interface use case Example

---
+++ src/com/learn/vikram/functionalInterfaces/BiFunctionExample.java
@@ -0,0 +1,28 @@
+package com.learn.vikram.functionalInterfaces;
+
+import com.learn.vikram.data.Student;
+import com.learn.vikram.data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+public class BiFunctionExample {
+
+  static BiFunction<List<Student>, Predicate<Student>, Map<String, Double>> biFunction = (students, studentPredicate)->{
+        Map<String, Double> studentGradeMap = new HashMap<>();
+        students.forEach(student->{
+            if(studentPredicate.test(student)){
+                studentGradeMap.put(student.getName(), student.getGpa());
+            }
+        });
+        return studentGradeMap;
+    };
+
+
+    public static void main(String[] args){
+      System.out.println(biFunction.apply(StudentDataBase.getAllStudents(), PredicateStudentExample.p2));
+    }
+}
